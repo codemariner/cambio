8670f33eb8d8b1da3d44a88702e2d1ac53e539c9
Rafal Dudek <rafal.dudek@teamaol.com>
2013-08-30 16:48:56 +0100

    Final intercept code

--- src/cambio.intercept.js
+++ src/cambio.intercept.js
@@ -33,40 +33,39 @@ var cambioIntercept = {
     
     checkAndDisplayIntercept : function () {
         //Check if cookie set
-        var cookie = this.getCookie('cambioIntercept');
-        //for user who already liked/followed 
-         //If not set it
-        if (cookie === null) {
-            this.setCookie('cambioIntercept', 101 - this.postNum, this.cookieExpire);
-            return true;
-        } else {
-            cookie = parseInt(cookie, 10);
+        if (typeof(this.active) !== 'undefined' && this.active === '1') {
+            var cookie = this.getCookie('cambioIntercept');
+            //for user who already liked/followed 
+             //If not set it
+            if (cookie === null) {
+                this.setCookie('cambioIntercept', 101 - this.postNum, this.cookieExpire);
+                return true;
+            } else {
+                cookie = parseInt(cookie, 10);
+            }
+            if (cookie === 200) {
+                return true;
+            }
+            //Display message if reached 100
+            if (cookie + 1 >= 100) {
+                this.displayIntercept();
+            } else {
+                this.setCookie('cambioIntercept', cookie + 1, this.cookieExpire); 
+            }
         }
-        if (cookie === 200) {
-            return true;
-        }
-       
-        //Display message if reached 100
-        if (cookie + 1 >= 100) {
-            this.displayIntercept();
-        } else {
-            this.setCookie('cambioIntercept', cookie + 1, this.cookieExpire); 
-        }
-        
-        
     },
     
     //When user click remain me later reset counter
     postponeIntercept : function () {
         this.setCookie('cambioIntercept', 100 - this.postponePostNum, this.cookieExpire);
-        this.recordUserAction(3);
+        this.recordUserAction(4);
         this.removeIntercept();
     },
     
     //when user already liked cambio
     alreadyLikeIntercept : function () {
         this.setCookie('cambioIntercept', 200, this.cookieExpire);
-        this.recordUserAction(2);
+        this.recordUserAction(3);
         this.removeIntercept();
     },
     
@@ -80,16 +79,41 @@ var cambioIntercept = {
         
         FB.Event.unsubscribe('edge.create',  cambioIntercept.likeIntercept);
     },
-    getFirstTag : function () {
-        return 'First Tag';
-        
+    
+    //When user click follow
+    followIntercept : function () {
+        //Have to use class name instead of this because this doesn't work with tw and fb button callbacs
+        cambioIntercept.setCookie('cambioIntercept', 200, this.cookieExpire);
+        //Send request to count user action
+        cambioIntercept.recordUserAction(2);
+        cambioIntercept.removeIntercept();    
     },
     
-    displayIntercept : function () {
-        var code = '<div id="cambioIntercept"><h2>Like of Follow us to stay up to date on the latest happenings with <span>' + this.getFirstTag() + '</span></h2>';
+    capitaliseFirstLetters : function (str) {
+        var words = str.split(' ');
+        var html = '';
+        $.each(words, function () {
+            var first_letter = this.substring(0, 1);
+            html += first_letter.toUpperCase() + this.substring(1) + ' ';
+        });
+        return html;
+    },
+    
+    getFirstTag : function () {
+        var tag = 'Cambio';
+        if ($('.relatedTags a:first').html()) {
+            tag = this.capitaliseFirstLetters($('.relatedTags a:first').html());
+        }
+        return tag;
+    },
+    
+    displayIntercept : function () {  
+        var code = '<div id="cambioIntercept"><h2>Like or Follow us to be first to know when news breaks on <span>' + this.getFirstTag() + '</span></h2>';
         code += '<div class="likeButtons"></div>';
         code += '<div class="buttons"><ul><li><a id="interceptAlready" href="#">I already like or follow Cambio</a></li><li><a id="interceptNo" href="#">No thanks</a></li></ul></div></div>';
         $('body').eq(0).append(code);
+        $('#cambioIntercept').css('left', $(window).width() / 2 - $('#cambioIntercept').width()  + 80 + 'px');
+        //$('#cambioIntercept').css('top', $(window).height() / 2 - $('#cambioIntercept').height() / 2 + 'px');
         this.addShare();
         var that = this;
         //add code to buttons
@@ -102,11 +126,10 @@ var cambioIntercept = {
             that.postponeIntercept();
         });
         $('#cambioIntercept').fadeIn(this.animationSpeen);
-        
     },
     
     addShare : function () {
-        $('#cambioIntercept .likeButtons').html('<ul><li><fb:like layout="box_count" href="https://www.facebook.com/cambioconnect" ref="intercept"></fb:like></li><li><a href="https://twitter.com/cambio" class="twitter-follow-button twitter-count-vertical" data-show-count="true" data-count="vertical" data-size="large" data-show-screen-name="false" data-dnt="true"></a></li></ul>');
+        $('#cambioIntercept .likeButtons').html('<ul><li><fb:like layout="button_count" href="https://www.facebook.com/cambioconnect" ref="intercept"></fb:like></li><li><a href="https://twitter.com/cambio" class="twitter-follow-button" data-show-count="true"  data-show-screen-name="false" data-dnt="true"></a></li></ul>');
         //Facebook
         if (typeof(FB) !== 'undefined') {
             FB.XFBML.parse($('.videoShare')[0]);
@@ -124,10 +147,10 @@ var cambioIntercept = {
         //Twitter
         if (typeof(twttr) !== 'undefined') {
             twttr.widgets.load();
-            twttr.events.bind('follow', that.likeIntercept);
+            twttr.events.bind('follow', that.followIntercept);
         } else {
             $.getScript('http://platform.twitter.com/widgets.js', function () {
-                twttr.events.bind('follow', that.likeIntercept);
+                twttr.events.bind('follow', that.followIntercept);
             });
         }
     },
@@ -140,6 +163,8 @@ var cambioIntercept = {
     
     recordUserAction : function (action) {
         //Send request to extension server
+        //1 - like, 2 - follow, 3 - already like, 4 - no thanks
+        
         $.ajax({
             dataType : 'jsonp',
             crossDomain : true,
