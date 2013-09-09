7b33c3620e55f0a9d339c898d654e4a5b0323947
Benjamin Manns <benmanns@gmail.com>
2013-09-09 15:12:02 -0400

    Activate Livefyre for social stream tab.

--- src/cambio.tagtabs.js
+++ src/cambio.tagtabs.js
@@ -14,6 +14,33 @@ $(function () {
         var selector = $this.data('tag-body-selector');
         switchTagBody(selector);
 
+        if ($this.data('comments')) {
+            $('#social-comments-disabled').attr('id', 'post-comments');
+
+            if (cambio.postComments.$commentsEl) {
+                $('#post-comments').appendTo(this.$commentsEl);
+            }
+            setTimeout(function () {
+                cambio.postComments.load();
+            }, 500);
+
+            var $el = $('#post-comments .fyre');
+            if ($el.length) {
+                cambio.postComments.$commentsEl = $el;
+            }
+            // just in case the comment stream was stopped, ask it to resume
+            if (cambio.postComments.widget) {
+                cambio.postComments.widget.resume();
+            }
+        }
+        else {
+            cambio.postComments.$commentsEl = $('#post-comments .fyre');
+            if (cambio.postComments.widget) {
+                cambio.postComments.widget.stop();
+            }
+            $('#post-comments').attr('id', 'social-comments-disabled');
+        }
+
         $parent.siblings('li.active').removeClass('active');
         $parent.addClass('active');
     });
