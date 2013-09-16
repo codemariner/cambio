0d5740b733272d72618a029f04c810b1fe0ec0a8
Rafal Dudek <rafal.dudek@teamaol.com>
2013-09-16 15:38:59 +0100

    Faceoff message

--- src/cambio.faceoff.js
+++ src/cambio.faceoff.js
@@ -8,27 +8,26 @@ var cambioFaceOff = {
     pages : [],
     userVotes : [],
     fbAppId : '1431201983771924',
-    fbAppSecret : '21b6858f55e4f92245ab3087854488a5',
     
     //Initialize object and display it
     init : function () {
         //Check if faceoff elements on the page
-        console.log('Faceoff init');
-        //Check if hash set
-        if (window.location.hash) {
-            console.log(window.location.hash);
-            this.currentPage = parseInt(window.location.hash.replace('#', ''), 10);
-            console.log(this.currentPage);
-        } 
-        var that = this;
-        if (this.getSlides()) {
-            this.getVotes();
-            this.getUserVotes();
-            $(this).on('cambio.faceoff.gotVotes', function () {
-                that.createPages();
-                that.displayCurrentPage();
-            });
-            this.initButtons();
+        if ($('.faceOff').length) {
+            //Check if hash set
+            if (window.location.hash) {
+                this.currentPage = parseInt(window.location.hash.replace('#', ''), 10);
+            } 
+            var that = this;
+            if (this.getSlides()) {
+                this.createPages();
+                this.createPageContainers();
+                this.getVotes();
+                this.getUserVotes();
+                $(this).on('cambio.faceoff.gotVotes', function () { 
+                    that.displayCurrentPage();
+                });
+                this.initButtons();
+            }
         }
     },
     
@@ -64,8 +63,6 @@ var cambioFaceOff = {
                     votes : 0  
                 };
             });
-            console.log('Data');
-            console.log(this.slides);
             return true;
         } else {
             return false;
@@ -75,7 +72,6 @@ var cambioFaceOff = {
     //Gets votes from external database
     getVotes : function () {
         var that = this;
-        console.log('Requesting data');
         $.ajax({
             url : '/faceoffresults/?faceoff=' + that.galleryId,
             type : 'GET',
@@ -86,7 +82,6 @@ var cambioFaceOff = {
                 var i = 0;
                 for (i; i < that.slides.length; i++) {
                     if (typeof(res[i]) !== 'undefined') {
-                        console.log('Result into slides');
                         that.slides[i].votes = parseInt(res[i], 10);
                     }
                 }
@@ -107,16 +102,22 @@ var cambioFaceOff = {
         //get data from cookie
         var cookie = $.cambio.getCookie('faceoff');
         if (cookie !== null) {
-            console.log('Cookie');
-            console.log(cookie);  
             this.userVotes = JSON.parse(cookie);
-            console.log(this.userVotes);
+        } else {
+            this.displayInstructionBox();
         }
     },    
     
+    createPageContainers : function () {
+        var i = 0;
+        for (i = 0; i < this.pages.length; i++) {
+            $('.faceOffPages').append('<li><div class="faceOffPage" id="faceOffPage_' + i + '"></div></li>');
+        }
+        $('.faceOffPage').css('width', $('.faceOffCarouselCnt').width());
+    },
+    
     //Saves vote into user cookie
-    voteToCookie : function (imageNum) {
-        this.userVotes[imageNum] = 1;
+    votesToCookie : function () {
         $.cambio.setCookie('cambiofaceoff', JSON.stringify(this.userVotes), 365);
     },
         
@@ -162,18 +163,16 @@ var cambioFaceOff = {
         //hide all pages
         this.currentPage = num;
         var that = this;
-        $('.faceOffPage:visible').fadeOut(this.animationSpeed, function () {
-            that.displayCurrentPage();
-        });
-        
+        that.displayCurrentPage(); 
     },
     
     //Displays page pointed by internal data
     displayCurrentPage : function () {
-        if ($('#faceOffPage_' + this.currentPage).length) {
-            $('#faceOffPage_' + this.currentPage).fadeIn(this.animationSpeed); 
+        if ($('#faceOffPage_' + this.currentPage + ' div').length) {
+            this.animateToCurrentPage();
+            //$('#faceOffPage_' + this.currentPage).fadeIn(this.animationSpeed); 
         } else {
-            var h = '<li><div class="faceOffPage" id="faceOffPage_' + this.currentPage + '">';
+            var h = '';
             var i = 0;
             var slide = 0;
             var voted = 0;
@@ -189,14 +188,14 @@ var cambioFaceOff = {
                 }
                 h += '<div class="faceOffSlide" id="faceOffSlide_' + this.slides[slide].index + '"><img src="' + this.slides[slide].url + '" /><div class="faceOffSlideDetails"><div class="credits credits' + i + '">' + this.slides[slide].credits + '</div><h3>' + this.slides[slide].title + '</h3><div class="text">' + this.slides[slide].text + '</div></div></div>';
             }
-            h += '</div></li>';
-            $('#faceOff_' + this.galleryId + ' .faceOffPages').append(h);
+            $('#faceOff_' + this.galleryId + ' #faceOffPage_' + this.currentPage).append(h);
+            //Move it to
             //If user didn't vote
             if (voted === 0) {
                 //Add vote buttons
                 for (i = 0; i < this.pages[this.currentPage].length; i++) {
                     slide = this.pages[this.currentPage][i];
-                    $('#faceOffSlide_' + this.slides[slide].index + ' .faceOffSlideDetails').prepend('<a href="#" id="voteButton_' + this.slides[slide].index + '" class="voteButton">Vote!</a>');
+                    $('#faceOffSlide_' + this.slides[slide].index + ' .faceOffSlideDetails').prepend('<a href="#" id="voteButton_' + this.slides[slide].index + '" class="voteButton"></a>');
                 }
                 //Add actions to button
                 var that = this;
@@ -206,25 +205,110 @@ var cambioFaceOff = {
                 });    
             } else {
                 //Display votes
-                this.displayVotes(votedForImage, 0);
+                this.displayVotes();
             }
         }
+        this.animateToCurrentPage();
         //Update page counter
         $('#faceOff_' + this.galleryId + ' .faceOffCounter .num').html(this.currentPage + 1);
         //Update URL (add hash)
         window.location.hash = '#' + this.currentPage;
-        console.log('Equalize box');
         this.equalizeImageBoxes();
     },
     
+    displayInstructionBox : function () {
+        console.log('Display instructions box');
+        //request body
+        var that = this;
+        $.ajax({
+            url : '/faceoffinstructions/',
+            type : 'GET',
+            success : function (code) { 
+                $('body').eq(0).append(code);
+                $('body').eq(0).append('<div class="cambioPopupBg" id="faceOffInstrBg"></div>');
+                $('#faceOffInstr').css('left', $(window).width() / 2 - $('#faceOffInstr').width()  + 80 + 'px');
+                //add code to buttons
+                $('#faceOffInstrClose').click(function (event) {
+                    event.preventDefault();
+                    that.closeInstructionBox();
+                });
+                $('#faceOffInstr, #faceOffInstrBg').fadeIn(that.animationSpeed);
+                //Add share buttons
+                that.addShare();
+            }
+        });
+    },
+    
+    addShare : function () {
+        //Facebook
+        var that = this;
+        var code = '<div class="fb-like" data-href="' + window.location.href + '" data-send="false" data-layout="button_count" data-show-faces="false"></div>';
+        $('#faceOffInstr li').eq(0).html(code);
+        if (typeof(FB) !== 'undefined') {
+            FB.XFBML.parse($('#faceOffInstr')[0]);
+            FB.Event.subscribe('edge.create',  cambioFaceOff.closeInstructionBox);
+        } else {
+            $.getScript("http://connect.facebook.net/en_US/all.js#xfbml=1", function () {
+                FB.XFBML.parse($('#faceOffInstr')[0]);
+                FB.Event.subscribe('edge.create',  cambioFaceOff.closeInstructionBox);
+            });
+        }
+        //Twitter
+        code = '<a href="https://twitter.com/share" class="twitter-share-button" data-url="' + window.location.href + '" href="http://twitter.com/share" class="twitter-share-button" data-via="cambio" data-count="horizontal">Tweet</a>';
+        $('#faceOffInstr li').eq(1).html(code);
+        if (typeof(twttr) !== 'undefined') {
+            twttr.widgets.load();
+            twttr.events.bind('tweet', cambioFaceOff.closeInstructionBox);
+        } else {
+            $.getScript('http://platform.twitter.com/widgets.js', function () {
+                twttr.events.bind('tweet', cambioFaceOff.closeInstructionBox);
+            });
+        } 
+        //Google
+        code = '<g:plus action="share" data-annotation="bubble"></g:plus>';
+        $('#faceOffInstr li').eq(2).html(code);
+        var gObj = {
+            "href" : window.location.href,
+            "callback" : cambioFaceOff.closeInstructionBox
+        };
+        if (typeof(gapi) !== 'undefined') {
+            $('#faceOffInstr .gPlusShare').each(function () {
+                gapi.plusone.render($(this).get(0), gObj);
+            });
+        } else {
+            window.___gcfg = {
+                lang : 'en-US',
+                parsetags : 'explicit'
+            };
+            $.getScript('https://apis.google.com/js/plusone.js', function () {
+                $('#faceOffInstr .gPlusShare').each(function () {
+                    gapi.plusone.render($(this).get(0), gObj);
+                });
+            });
+        }
+    },
+    
+    closeInstructionBox : function (obj) {
+        console.log('Closing instruction box');
+        $('#faceOffInstr, #faceOffInstrBg').fadeOut(this.animationSpeed, function () {
+            $('#faceOffInstr, #faceOffInstrBg').remove();
+        });
+        this.votesToCookie();
+    },
+    
+    animateToCurrentPage : function () {
+        var left = $('#faceOffPage_' + this.currentPage).position().left * -1;
+        $('.faceOffPages').animate({left : left + 'px'});
+    },
+    
     //Displays votes on current page after vote
-    displayVotes : function (imageNum) { 
+    displayVotes : function () { 
         $('#faceOffPage_' + this.currentPage + ' .voteButton').remove();
-        $('#faceOffPage_' + this.currentPage + ' #faceOffSlide_' + imageNum + ' .faceOffSlideDetails').prepend('<div class="voted">VOTED!</div>'); 
+       
         var i = 0;
         var slide = 0;
         var votes = 0;
-        var vWord = '';
+        var vWord = '', elemClass = '', bgClass = '';
         for (i = 0; i < this.pages[this.currentPage].length; i++) {
             slide = this.pages[this.currentPage][i];
             votes = this.slides[slide].votes;
@@ -232,14 +316,22 @@ var cambioFaceOff = {
             if (votes === 1) {
                 vWord = 'vote';
             }
-            $('#faceOffPage_' + this.currentPage + ' #faceOffSlide_' + slide + ' .faceOffSlideDetails').append('<div class="votes">' + votes + ' ' + vWord + '</div>'); 
+            
+            if (this.userVotes[slide] === 1) {
+                elemClass = 'voted';   
+                bgClass = '';     
+            } else {
+                elemClass = 'notVoted';
+                bgClass = 'noVote';
+            }
+            $('#faceOffPage_' + this.currentPage + ' #faceOffSlide_' + slide + ' .faceOffSlideDetails').prepend('<div class="' + elemClass + '"></div>'); 
+            $('#faceOffPage_' + this.currentPage + ' #faceOffSlide_' + slide + ' .faceOffSlideDetails').prepend('<div class="votes ' + bgClass + '"><div class="voteNum">' + votes + ' ' + vWord + '</div><div class="voteArrow"></div></div>'); 
         }
         this.equalizeImageBoxes();
     },
     
     //Sets the same height for imageBoxes on current page
     equalizeImageBoxes : function () {
-        console.log('Equalizing image boxes');
         $('#faceOffPage_' + this.currentPage + ' .faceOffSlideDetails').css('height', 'inherit');
         var height = 0;
         $('#faceOffPage_' + this.currentPage + ' .faceOffSlideDetails').each(function () {
@@ -247,21 +339,22 @@ var cambioFaceOff = {
                 height = $(this).height();
             }
         });
-        console.log(height);
         $('#faceOffPage_' + this.currentPage + ' .faceOffSlideDetails').css('height', height + 'px');
+        $('.faceOffCarouselCnt').css('height', $('#faceOffPage_' + this.currentPage + ' .faceOffSlide').height() + 15 + 'px');
     },
     
     //All action related to vote
     vote : function (imageNum) {      
         this.slides[imageNum].votes += 1;
+        this.userVotes[imageNum] = 1;
         //Send data to database
         this.sendVote(imageNum);
-        //Record action in user cookie
-        this.voteToCookie(imageNum);
+        //Record votes in user cookie
+        this.votesToCookie();
         //Send data to user's facebook wall
         this.publishToFBWall(imageNum);
         //Display vote result numbers
-        this.displayVotes(imageNum, 1);
+        this.displayVotes();
     },
        
     //sends vote to database
@@ -275,10 +368,10 @@ var cambioFaceOff = {
         });
     },
     
-    //TODO send to user facebook wall
+    //TODO send to user facebook wall for now suspended
     publishToFBWall : function (imageNum) {
         var message = 'I just voted for ' + this.slides[imageNum].title + '! ' + this.fbMessage;
-        window.alert('Send request to facebook - ' + message);
+        console.log('Send request to facebook - ' + message);
     }
     
 };
